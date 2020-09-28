303d00250bd4268dd4b99621d1e0f3a93b983ae3
abc <123456@qq.com>
2020-09-28 15:32:50 +0800

    fz21

--- src/main/resources/cn/kgc/git/Git1.java
+++ src/main/resources/cn/kgc/git/Git1.java
@@ -8,8 +8,8 @@ System.out.println("123456");
 System.out.println("147258");
 System.out.println("zswxde");
 System.out.println("rfctgv");
-System.out.println("11111111");
 System.out.println("2222222222");
+System.out.println("4444444444");
 	}
 
 }
