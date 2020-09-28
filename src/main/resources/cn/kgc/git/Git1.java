525a753496c35cdd4791446c5bdebfb50fbaab8a
abc <123456@qq.com>
2020-09-28 19:21:50 +0800

    aaa

--- src/main/resources/cn/kgc/git/Git1.java
+++ src/main/resources/cn/kgc/git/Git1.java
@@ -4,17 +4,14 @@ public class Git1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("123456");
-System.out.println("147258");
-System.out.println("zswxde");
-System.out.println("rfctgv");
-<<<<<<< HEAD
-System.out.println("11111111");
-System.out.println("33333333");
-=======
-System.out.println("2222222222");
-System.out.println("4444444444");
-		System.out.println("4444444444");
->>>>>>> refs/heads/Dev2
+	System.out.println("123456");
+	System.out.println("147258");
+	System.out.println("zswxde");
+	System.out.println("rfctgv");
+	System.out.println("111111");
+	System.out.println("333333");
+	System.out.println("222222");
+	System.out.println("444444");
+	System.out.println("444444");
 	}
 }
